baaa2d8ca3d5cb9f682ed8e371b65cba23d343b2
22012942 <22012942@LAPTOP-GR5M7NFA>
2023-08-10 23:13:47 +0800

    Option 5 done by xavier

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,240 @@
 
-public class C206_CaseStudy {
+	import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class C206_CaseStudy {
 
-	}
+	 public static void main(String[] args) {
+	  
+	  ArrayList<User> userList = new ArrayList<User>();
+	  ArrayList<Enrolment> enrolmentList = new ArrayList<Enrolment>();
+	  
+	  
+	  userList.add(new User("U01", "G V Timothy", "Administrator"));
+	  userList.add(new User("U02", "Toby Oh", "Administrator"));
+	  
+	  enrolmentList.add(new Enrolment("E01", "G V Timothy","Science","11/04/2023"));
+	  enrolmentList.add(new Enrolment("E02", "Toby Oh","Math","11/08/2023"));
+	  
+	  int option = 0;
+	  
+	  while (option != 7) {
+	   
+	   C206_CaseStudy.menu();
+	   option = Helper.readInt("Enter an option > ");
+	   
+	   if (option == 1) {
+	    
+	    int detailType = 0;
+	    
+	    while (detailType != 4) {
+	     
+	     C206_CaseStudy.setHeader("USERS");   
+	     C206_CaseStudy.setHeader("DETAIL TYPES");
+	     System.out.println("1. Add a new user");
+	     System.out.println("2. View all users");
+	     System.out.println("3. Delete an existing user");
+	     System.out.println("4. Quit");
+	     
+	     detailType = Helper.readInt("\nEnter option to select detail type > ");
+	     
+	     if (detailType == 1) {
+	      // Add a a new user
+	      User u = inputUser();
+	      C206_CaseStudy.addUser(userList, u);
+	     } else if (detailType == 2) {
+	      // View all users
+	      C206_CaseStudy.viewAllUser(userList);
+	     }  else if (detailType == 3) {
+	      // Delete an existing user
+	      String id = Helper.readString("Enter user id > ");
+	      C206_CaseStudy.deleteUser(userList, id);
+	     } else if (detailType == 4) {
+	      System.out.println("Thank you!");
+	     } else {
+	      System.out.println("Invalid option");
+	     } 
+	    }
+	    
+	   } else if (option == 2) {
+	    // Li Hua
+	   } else if (option == 3) {
+	    // Jian Hong 
+	   } else if (option == 4) {
+	    // Ryan
+	   } else if (option == 5) {
+		   int detailType = 0;
+		    
+		    while (detailType != 4) {
+		     
+		     C206_CaseStudy.setHeader("ENROLMENTS");   
+		     C206_CaseStudy.setHeader("DETAIL TYPES");
+		     System.out.println("1. Add a new Enrolemnt");
+		     System.out.println("2. View all Enrolments");
+		     System.out.println("3. Delete an existing enrolment");
+		     System.out.println("4. Quit");
+		     
+		     detailType = Helper.readInt("\nEnter option to select detail type > ");
+		     
+		     if (detailType == 1) {
+		      // Add a a new user
+		      Enrolment e = inputEnrolment();
+		      C206_CaseStudy.addEnrolment(enrolmentList, e);
+		     } else if (detailType == 2) {
+		      // View all users
+		      C206_CaseStudy.viewAllEnrolment(enrolmentList);
+		     }  else if (detailType == 3) {
+		      // Delete an existing user
+		      String id = Helper.readString("Enter user id > ");
+		      C206_CaseStudy.deleteEnrolment(enrolmentList, id);
+		     } else if (detailType == 4) {
+		      System.out.println("Thank you!");
+		     } else {
+		      System.out.println("Invalid option");
+		     } 
+		    }
+	   } else if (option == 6) {
+	    // Toby
+	   } else if (option == 7) {
+	    System.out.println("Bye!");
+	   } else {
+	    System.out.println("Invalid option");
+	   }
+	  }
+	 }
+	 
+	 public static void menu() {
+	  C206_CaseStudy.setHeader("TUITION MANAGEMENT APP");
+	  System.out.println("1. Manage Users");
+	  System.out.println("2. Manage Courses");
+	  System.out.println("3. Manage Students");
+	  System.out.println("4. Manage Fees");
+	  System.out.println("5. Manage Enrolments");
+	  System.out.println("6. Manage Attendance");
+	  System.out.println("7. Quit");
+	  Helper.line(80, "-");
+	 }
+	 
+	 public static void setHeader(String header) {
+	  Helper.line(80, "-");
+	  System.out.println(header);
+	  Helper.line(80, "-");
+	 }
+	 
+	 //================================= Option 1 Manage users =================================
+	 
+	 public static User inputUser() {
+	  String id = Helper.readString("Enter user id > ");
+	  String name = Helper.readString("Enter name > ");
+	  String role = Helper.readString("Enter role > ");
 
+	  User u= new User(id, name, role);
+	  return u;
+	  
+	 }
+	 
+	 public static void addUser(ArrayList<User> userList, User u) {
+	  User detail;
+	  for(int i = 0; i < userList.size(); i++) {
+	   detail = userList.get(i);
+	   if (detail.getId().equalsIgnoreCase(u.getId()) )
+	    return;
+	  }
+	  if ((u.getId().isEmpty()) || (u.getName().isEmpty()) ) {
+	   System.out.println("User id or name cannot be empty");
+	   return;
+	  } else {
+	   userList.add(u);
+	   System.out.println("User added");
+	  }
+	 }
+	 
+	 public static String retrieveAllUser(ArrayList<User> userList) {
+	  String output = "";
+
+	  for (int i = 0; i < userList.size(); i++) {
+	   output += String.format("%-10s %-20s %-20s\n", userList.get(i).getId(), 
+	     userList.get(i).getName(), userList.get(i).getRole());
+	  }
+	  return output;
+	 }
+	 
+	 public static void viewAllUser(ArrayList<User> userList) {
+	  C206_CaseStudy.setHeader("USER LIST");
+	  String output = String.format("%-10s %-20s %-20s\n", "USER ID", "NAME",
+	    "ROLE");
+	   output += retrieveAllUser(userList); 
+	  System.out.println(output);
+	 }
+	 
+	 public static void deleteUser(ArrayList<User> userList, String id) {
+	  for(int i = 0; i < userList.size(); i++) {
+	   User detail = userList.get(i);
+	   if (detail.getId().equalsIgnoreCase(id)) {
+	    userList.remove(i);
+	    System.out.println("User deleted");
+	    return;
+	   }
+	  }
+	  System.out.println("User not found");
+	 }
+	 
+	 //================================= Option 5 Manage enrolment =================================
+	 
+	 public static Enrolment inputEnrolment() {
+		  String id = Helper.readString("Enter Enrolment id > ");
+		  String name = Helper.readString("Enter name > ");
+		  String courseName = Helper.readString("Enter Course name > ");
+		  String enrolmentDate = Helper.readString("Enter Enrolment Date > ");
+
+		  Enrolment e= new Enrolment(id, name, courseName,enrolmentDate);
+		  return e;
+		  
+		 }
+		 
+		 public static void addEnrolment(ArrayList<Enrolment> enrolmentList, Enrolment e) {
+		  Enrolment detail;
+		  for(int i = 0; i < enrolmentList.size(); i++) {
+		   detail =enrolmentList.get(i);
+		   if (detail.getId().equalsIgnoreCase(e.getId()) )
+		    return;
+		  }
+		  if ((e.getId().isEmpty()) || (e.getName().isEmpty()) ) {
+		   System.out.println("User id or name cannot be empty");
+		   return;
+		  } else {
+		   enrolmentList.add(e);
+		   System.out.println("Enrolment added");
+		  }
+		 }
+		 
+		 public static String retrieveAllEnrolment(ArrayList<Enrolment> enrolmentList) {
+		  String output = "";
+
+		  for (int i = 0; i < enrolmentList.size(); i++) {
+		   output += String.format("%-20s %-20s %-20s %-20s\n", enrolmentList.get(i).getId(), 
+		     enrolmentList.get(i).getName(), enrolmentList.get(i).getCourseName(),enrolmentList.get(i).getEnrolmentDate());
+		  }
+		  return output;
+		 }
+		 
+		 public static void viewAllEnrolment(ArrayList<Enrolment> enrolmentList) {
+		  C206_CaseStudy.setHeader("ENROLMENT LIST");
+		  String output = String.format("%-20s %-20s %-20s %-20s\n", "ENROLMENT ID", "NAME",
+		    "COURSE NAME","ENROLMENT DATE");
+		   output += retrieveAllEnrolment(enrolmentList); 
+		  System.out.println(output);
+		 }
+		 
+		 public static void deleteEnrolment(ArrayList<Enrolment> enrolmentList, String id) {
+		  for(int i = 0; i < enrolmentList.size(); i++) {
+		   Enrolment detail = enrolmentList.get(i);
+		   if (detail.getId().equalsIgnoreCase(id)) {
+		    enrolmentList.remove(i);
+		    System.out.println("Enrolment deleted");
+		    return;
+		   }
+		  }
+		  System.out.println("Enrolment not found");
+		 }
+	 
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,213 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	
+	// Prepare test data
+	private User u01;
+	private User u02;
+	private User u03;
+	
+	private Enrolment e01;
+	private Enrolment e02;
+	private Enrolment e03;
+	
+	private ArrayList<User> userList;
+	private ArrayList<Enrolment> enrolmentList;
+	
+	public C206_CaseStudyTest() {
+		super();
+	}
 
 	@Before
 	public void setUp() throws Exception {
+		
+		// Prepare test data
+		u01 = new User("U01", "G V Timothy", "Administrator");
+		u02 = new User("U02", "Toby Oh", "Administrator");
+		u03 = new User("U03", "Ryan Loh", "Administrator");
+		
+		e01 = new Enrolment("E01", "G V Timothy","Science","11/04/2023");
+		e02 = new Enrolment("E02", "Toby Oh","Math","11/08/2023");
+		e03 = new Enrolment("E03", "Xavier","English","11/02/2023");
+		
+		userList= new ArrayList<User>();
+		enrolmentList = new ArrayList<Enrolment>();
+	}
+	
+	@Test
+	public void testAddUser() {
+		// User list is not null and it is empty
+		assertNotNull("Test if there is a valid User arraylist to add to", userList);
+		assertEquals("Test that the User arraylist is empty.", 0, userList.size());
+		// Given an empty list, after adding 1 user, the size of the list is 1
+		C206_CaseStudy.addUser(userList, u01);
+		assertEquals("Test that the User arraylist size is 1.", 1, userList.size());
+		
+		// Add a user
+		C206_CaseStudy.addUser(userList, u02);
+		assertEquals("Test that the User arraylist size is now 2.", 2, userList.size());
+		//The user just added is as same as the last item in the list
+		assertSame("Test that the User is added to the end of the list.", u02, userList.get(1));
+
+		// Add a user that already exists in the list
+		C206_CaseStudy.addUser(userList, u02);
+		assertEquals("Test that the User arraylist size is unchanged.", 2, userList.size());
+
+		// Add a user that has missing detail
+		User u_missing = new User("U03", "", "Administrator");
+		C206_CaseStudy.addUser(userList, u_missing);
+		assertEquals("Test that the User arraylist size is unchanged.", 2, userList.size());
+	}
+	
+	@Test
+	public void testRetrieveAllUser() {
+		// Test Case 1
+		// Test if User list is not null and empty
+		assertNotNull("Test if there is valid User arraylist to add to", userList);
+		assertEquals("Test that the User arraylist is empty.", 0, userList.size());
+		// Attempt to retrieve the Users
+		String allUser= C206_CaseStudy.retrieveAllUser(userList);
+		String testOutput = "";
+		// Test if the output is empty
+		assertEquals("Test that nothing is displayed", testOutput, allUser);
+
+		//Test Case 2
+		C206_CaseStudy.addUser(userList, u01);
+		// Test that the list is not empty
+		assertEquals("Test that User arraylist size is 1.", 1, userList.size());
+		// Attempt to retrieve the User
+		allUser= C206_CaseStudy.retrieveAllUser(userList);
+		testOutput = String.format("%-10s %-20s %-20s\n","U01", "G V Timothy", "Administrator");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allUser);
+
+		//Test Case 3
+		C206_CaseStudy.addUser(userList, u02);
+		C206_CaseStudy.addUser(userList, u03);
+		// Test that the list is not empty
+		assertEquals("Test that User arraylist size is 3.", 3, userList.size());
+		// Attempt to retrieve the User
+		allUser= C206_CaseStudy.retrieveAllUser(userList);
+		testOutput = String.format("%-10s %-20s %-20s\n","U01", "G V Timothy", "Administrator");
+		testOutput += String.format("%-10s %-20s %-20s\n","U02", "Toby Oh", "Administrator");
+		testOutput += String.format("%-10s %-20s %-20s\n","U03", "Ryan Loh", "Administrator");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allUser);
+	}
+	
+	@Test
+	public void testDeleteUser() {
+		// User list is not null and it is not empty
+		C206_CaseStudy.addUser(userList, u01);
+		C206_CaseStudy.addUser(userList, u02);
+		C206_CaseStudy.addUser(userList, u03);
+		assertNotNull("Test if there is a valid User arraylist to delete", userList);
+		assertEquals("Test that the User arraylist is not empty.", 3, userList.size());
+
+		// Delete a user
+		C206_CaseStudy.deleteUser(userList, "U02");
+		assertEquals("Test that the User arraylist size is now 2.", 2, userList.size());
+
+		// Delete the last user in the list
+		C206_CaseStudy.deleteUser(userList, "U03");
+		assertEquals("Test that the User arraylist size is now 1.", 1, userList.size());
+
+		// Delete a user that does not exist in the list
+		C206_CaseStudy.deleteUser(userList, "U04");
+		assertEquals("Test that the User arraylist size is unchanged.", 1, userList.size());
 	}
 
+	@Test
+	public void testAddEnrolment() {
+		// Enrolment list is not null and it is empty
+		assertNotNull("Test if there is a valid Enrolment arraylist to add to", enrolmentList);
+		assertEquals("Test that the Enrolment arraylist is empty.", 0, enrolmentList.size());
+		// Given an empty list, after adding 1 enrolment, the size of the list is 1
+		C206_CaseStudy.addEnrolment(enrolmentList, e01);
+		assertEquals("Test that the Enrolment arraylist size is 1.", 1, enrolmentList.size());
+		
+		// Add a enrolment
+		C206_CaseStudy.addEnrolment(enrolmentList, e02);
+		assertEquals("Test that the Enrolment arraylist size is now 2.", 2, enrolmentList.size());
+		//The enrolment just added is as same as the last item in the list
+		assertSame("Test that the Enrolment is added to the end of the list.", e02, enrolmentList.get(1));
+
+		// Add an enrolment that already exists in the list
+		C206_CaseStudy.addEnrolment(enrolmentList, e02);
+		assertEquals("Test that the Enrolment arraylist size is unchanged.", 2, enrolmentList.size());
+
+		// Add a user that has missing detail
+		Enrolment e_missing = new Enrolment("E03", "", "English", "11/02/2023");
+		C206_CaseStudy.addEnrolment(enrolmentList, e_missing);
+		assertEquals("Test that the Enrolment arraylist size is unchanged.", 2, enrolmentList.size());
+	}
+	
+	@Test
+	public void testRetrieveAllEnrolment() {
+		// Test Case 1
+		// Test if Enrolment list is not null and empty
+		assertNotNull("Test if there is valid Enrolment arraylist to add to", enrolmentList);
+		assertEquals("Test that the Enrolment arraylist is empty.", 0, enrolmentList.size());
+		// Attempt to retrieve the Enrolments
+		String allEnrolment= C206_CaseStudy.retrieveAllEnrolment(enrolmentList);
+		String testOutput = "";
+		// Test if the output is empty
+		assertEquals("Test that nothing is displayed", testOutput, allEnrolment);
+
+		//Test Case 2
+		C206_CaseStudy.addEnrolment(enrolmentList, e01);
+		// Test that the list is not empty
+		assertEquals("Test that Enrolment arraylist size is 1.", 1, enrolmentList.size());
+		// Attempt to retrieve the enrolment
+		allEnrolment= C206_CaseStudy.retrieveAllEnrolment(enrolmentList);
+		testOutput = String.format("%-20s %-20s %-20s %-20s\n","E01", "G V Timothy", "Science","11/04/2023");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allEnrolment);
+
+		//Test Case 3
+		C206_CaseStudy.addEnrolment(enrolmentList, e02);
+		C206_CaseStudy.addEnrolment(enrolmentList, e03);
+		// Test that the list is not empty
+		assertEquals("Test that Enrolment arraylist size is 3.", 3, enrolmentList.size());
+		// Attempt to retrieve the User
+		allEnrolment= C206_CaseStudy.retrieveAllEnrolment(enrolmentList);
+		testOutput = String.format("%-20s %-20s %-20s %-20s\n","E01", "G V Timothy", "Science","11/04/2023");
+		testOutput += String.format("%-20s %-20s %-20s %-20s\n","E02", "Toby Oh", "Math","11/08/2023");
+		testOutput += String.format("%-20s %-20s %-20s %-20s\n","E03", "Xavier", "English","11/02/2023");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allEnrolment);
+	}
+	
+	@Test
+	public void testDeleteEnrolment() {
+		// Enrolment list is not null and it is not empty
+		C206_CaseStudy.addEnrolment(enrolmentList, e01);
+		C206_CaseStudy.addEnrolment(enrolmentList, e02);
+		C206_CaseStudy.addEnrolment(enrolmentList, e03);
+		assertNotNull("Test if there is a valid Enrolment arraylist to delete", enrolmentList);
+		assertEquals("Test that the Enrolment arraylist is not empty.", 3, enrolmentList.size());
+
+		// Delete a enrolment
+		C206_CaseStudy.deleteEnrolment(enrolmentList, "E02");
+		assertEquals("Test that the Enrolment arraylist size is now 2.", 2, enrolmentList.size());
+
+		// Delete the last enrolment in the list
+		C206_CaseStudy.deleteEnrolment(enrolmentList, "E03");
+		assertEquals("Test that the Enrolment arraylist size is now 1.", 1, enrolmentList.size());
+
+		// Delete a enrolment that does not exist in the list
+		C206_CaseStudy.deleteEnrolment(enrolmentList, "E04");
+		assertEquals("Test that the Enrolment arraylist size is unchanged.", 1, enrolmentList.size());
+	}
+	
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
-	}
-
-}
+} // end of class
