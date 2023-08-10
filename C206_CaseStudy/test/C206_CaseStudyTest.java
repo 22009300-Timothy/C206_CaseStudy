18bef169df4797311e63b8fc1991fc7f8f0a6751
22009300 <22009300@LAPTOP-K4ABEPHV>
2023-08-10 22:30:07 +0800

    Option 1 Done by Timothy

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,124 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	
+	// Prepare test data
+	private User u01;
+	private User u02;
+	private User u03;
+	
+	private ArrayList<User> userList;
+	
+	public C206_CaseStudyTest() {
+		super();
+	}
 
 	@Before
 	public void setUp() throws Exception {
+		
+		// Prepare test data
+		u01 = new User("U01", "G V Timothy", "Administrator");
+		u02 = new User("U02", "Toby Oh", "Administrator");
+		u03 = new User("U03", "Ryan Loh", "Administrator");
+		
+		userList= new ArrayList<User>();
+	}
+	
+	@Test
+	public void testAddUser() {
+		// User list is not null and it is empty
+		assertNotNull("Test if there is a valid User arraylist to add to", userList);
+		assertEquals("Test that the User arraylist is empty.", 0, userList.size());
+		// Given an empty list, after adding 1 user, the size of the list is 1
+		C206_CaseStudy.addUser(userList, u01);
+		assertEquals("Test that the User arraylist size is 1.", 1, userList.size());
+		
+		// Add a user
+		C206_CaseStudy.addUser(userList, u02);
+		assertEquals("Test that the User arraylist size is now 2.", 2, userList.size());
+		//The user just added is as same as the last item in the list
+		assertSame("Test that the User is added to the end of the list.", u02, userList.get(1));
+
+		// Add a user that already exists in the list
+		C206_CaseStudy.addUser(userList, u02);
+		assertEquals("Test that the User arraylist size is unchanged.", 2, userList.size());
+
+		// Add a user that has missing detail
+		User u_missing = new User("U03", "", "Administrator");
+		C206_CaseStudy.addUser(userList, u_missing);
+		assertEquals("Test that the User arraylist size is unchanged.", 2, userList.size());
+	}
+	
+	@Test
+	public void testRetrieveAllUser() {
+		// Test Case 1
+		// Test if User list is not null and empty
+		assertNotNull("Test if there is valid User arraylist to add to", userList);
+		assertEquals("Test that the User arraylist is empty.", 0, userList.size());
+		// Attempt to retrieve the Users
+		String allUser= C206_CaseStudy.retrieveAllUser(userList);
+		String testOutput = "";
+		// Test if the output is empty
+		assertEquals("Test that nothing is displayed", testOutput, allUser);
+
+		//Test Case 2
+		C206_CaseStudy.addUser(userList, u01);
+		// Test that the list is not empty
+		assertEquals("Test that User arraylist size is 1.", 1, userList.size());
+		// Attempt to retrieve the User
+		allUser= C206_CaseStudy.retrieveAllUser(userList);
+		testOutput = String.format("%-10s %-20s %-21s\n","U01", "G V Timothy", "Administrator");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allUser);
+
+		//Test Case 3
+		C206_CaseStudy.addUser(userList, u02);
+		C206_CaseStudy.addUser(userList, u03);
+		// Test that the list is not empty
+		assertEquals("Test that User arraylist size is 3.", 3, userList.size());
+		// Attempt to retrieve the User
+		allUser= C206_CaseStudy.retrieveAllUser(userList);
+		testOutput = String.format("%-10s %-20s %-21s\n","U01", "G V Timothy", "Administrator");
+		testOutput += String.format("%-10s %-20s %-21s\n","U02", "Toby Oh", "Administrator");
+		testOutput += String.format("%-10s %-20s %-21s\n","U03", "Ryan Loh", "Administrator");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allUser);
+	}
+	
+	@Test
+	public void testDeleteUser() {
+		// User list is not null and it is not empty
+		C206_CaseStudy.addUser(userList, u01);
+		C206_CaseStudy.addUser(userList, u02);
+		C206_CaseStudy.addUser(userList, u03);
+		assertNotNull("Test if there is a valid User arraylist to delete", userList);
+		assertEquals("Test that the User arraylist is not empty.", 3, userList.size());
+
+		// Delete a user
+		C206_CaseStudy.deleteUser(userList, "U02");
+		assertEquals("Test that the User arraylist size is now 2.", 2, userList.size());
+
+		// Delete the last user in the list
+		C206_CaseStudy.deleteUser(userList, "U03");
+		assertEquals("Test that the User arraylist size is now 1.", 1, userList.size());
+
+		// Delete a user that does not exist in the list
+		C206_CaseStudy.deleteUser(userList, "U04");
+		assertEquals("Test that the User arraylist size is unchanged.", 1, userList.size());
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		u01 = null;
+		u02 = null;
+		u03 = null;
+		userList = null;
 	}
 
-	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
-	}
-
-}
+} // end of class
